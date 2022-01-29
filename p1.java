d7ee3f8932a4eb6054d87367412965384f7b7bc5
mhanson8 <98630077+mhanson8@users.noreply.github.com>
2022-01-28 22:47:36 -0500

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,53 @@
+
+public class p1 {
+	
+	private int validcount;
+	private Boolean quit;
+	private Boolean newgame;
+	
+	//this is the constructor 
+	public p1(){
+		
+	}
+	
+	public void printarr(int[][] arr)
+	{
+		//this method will create the 4D array and print it when initially starting the game 
+		//this method will also print the array, with the original randomized numbers 
+		
+	
+		
+		
+	}
+	
+	public static int generatedigit()
+	{
+		double rand = Math.random();
+		if (rand > 0.8) return 2;
+		else return 4;
+	}
+	
+	public void incrementvalidcount()
+	{
+		validcount++;
+	}
+	
+	public int getvalidcount()
+	{
+		return validcount;
+	}
+	
+	public void runGame()
+	{
+		
+		
+		
+	}
+	
+	
+	
+	
+
+	
+	
+}
